656e62283f060158c49ff47a5d800c0bf3b7710f
uvatandost <uvatandost@my.harrisburgu.edu>
2021-04-01 17:16:41 -0400

    Fixing bugs

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly
+import seaborn as sns
 
 
 """
