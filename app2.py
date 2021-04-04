427f4cd1744cbd37243b79cc087917d050a2bc22
uvatandost <uvatandost@my.harrisburgu.edu>
2021-04-04 10:48:25 -0400

    Production

--- app2.py
+++ app2.py
@@ -10,65 +10,48 @@ import plotly.express as px
 import os
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('C:/Users/uvata/Projects/streamlit-framework')  # adjust as appropriate
+
+"""
+# TDI Milestone Project
+An interactive chart of stock closing prices
+"""
+
+st.sidebar.write("Please enter a ticker and select a date range:")
+
+project_folder = os.path.expanduser('C:/Users/uvata/Projects/streamlit-framework')
 load_dotenv(os.path.join(project_folder, '.env'))
 key = os.getenv("KEY")
 
-#st.write(key)
-#st.write(os.environ.get('KEY'))
-#st.write(os.environ['HOME'])
-#st.write(os.environ)
+# Input a ticker
+ticker = str(st.sidebar.text_input('Ticker:', 'GOOG'))
 
-ticker = 'AAP'
+# Fetching the data from API
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
-response = requests.get(url)
-response = response.json()
-#print(response)
 
+try:
+	@st.cache
+	def load_data():
+		response = requests.get(url)
+		response = response.json()
+		df = pd.DataFrame(response['Time Series (Daily)']).transpose()
+		return df
 
-df = pd.DataFrame(response['Time Series (Daily)']).transpose()
-df.columns = ["open", "high", "low", "close", "adjusted close",
-" volume", "divident amount", "split coefficient"]
+	df = load_data()
+	df.columns = ["open", "high", "low", "close", "adjusted close",
+	" volume", "divident amount", "split coefficient"]
 
-df = df.applymap(float) # converting datapoints from str to float
-df = df.reindex(index=df.index[::-1])
-st.write(df)
-df2 = df.iloc[:20]['close']
-'''
-p = figure(title= "Closing Price",x_axis_label='Date',x_axis_type='datetime')
-x = df.index
-y = df.close
-#x = np.arange(0,10)
-p.line(x, y, legend_label=ticker, line_width=2)
-st.bokeh_chart(p, use_container_width=False)
-'''
+	df = df.applymap(float) # converting datapoints from str to float
+	df = df.reindex(index=df.index[::-1])
+	start_date, end_date = st.sidebar.select_slider('Date Range:',
+	options=list(df.index),
+	value=(df.index[0], df.index[99]))
 
+	df = df[start_date:end_date]
 
-fig = px.line(df, x=df.index, y="close")
-st.plotly_chart(fig, use_container_width=True)
-
-
-'''
-sns.set_style('ticks')
-fig, ax = plt.subplots()
-fig.set_size_inches(4, 2)
-x = df.index[0:8]
-y = df.close
-sns.lineplot(df, x=x, y=y)
-st.pyplot()
-'''
-'''
-st.line_chart(df2, use_container_width=True, height=300)
-
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-p = figure(
-	title='simple line example',
-	x_axis_label='x',
-	y_axis_label='y')
-
-p.line(x, y, legend_label='Trend', line_width=2)
-st.bokeh_chart(p)
-show(p)
-'''
+	fig = px.line(df, x=df.index, y="close")
+	fig.update_layout(title=str(ticker.upper()), xaxis_title='Date',yaxis_title='Closing Price')
+	st.plotly_chart(fig, use_container_width=True)
+except KeyError:
+	st.write("Please enter a valid ticker (e.i. AAPL)")
+else:
+	pass
